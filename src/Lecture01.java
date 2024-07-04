f1bb6cc5738e4371109dd1662bd9446901b15277
Your Name <you@example.com>
2024-07-04 11:58:04 -0700

    Work In Progress

--- src/Lecture01.java
+++ src/Lecture01.java
@@ -11,13 +11,28 @@ class Lecture01 {
         // this program
         // (in other words, when you run this program, what does it print?
         // Copy that output into this file, right here, in a comment
+        // "Hello from lecture 01" is the output the program produces when I first download it.
 
         // Where in the textbook are the main topics for this question covered?
+        // This is from all the way back in Chapter 1.1 in the book.
 
         // Put your answer for #2 here:
+        int endPoint = 100;
+        for (int i = 1; i <= endPoint; i++) {
+            System.out.print(i + " ");
+        }
         // Where in the textbook are the main topics for this question covered?
+        // This concept is from chapter 2.3.
+        System.out.println();
 
         // Put your answer for #3 here:
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         // Where in the textbook are the main topics for this question covered?
 
         // Put your answer for #4 here:
